ee48250417a7f2a0ba4d77707e0cb6bf84eea30a
Comfee <Comfee@DESKTOP-9RCD133>
2024-09-10 13:59:08 +0200

    created an application of numbers

--- src/main/java/sgela/git/GIT.java
+++ src/main/java/sgela/git/GIT.java
@@ -11,6 +11,7 @@ package sgela.git;
 public class GIT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 5;
+        int b = 10;
     }
 }
