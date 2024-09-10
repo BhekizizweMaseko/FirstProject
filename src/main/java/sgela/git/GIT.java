511db94fc85cbd0704f519aeba51aae5d30ab171
Comfee <Comfee@DESKTOP-9RCD133>
2024-09-10 13:26:22 +0200

    First Commit Nigga

---
+++ src/main/java/sgela/git/GIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sgela.git;
+
+/**
+ *
+ * @author Comfee
+ */
+public class GIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
